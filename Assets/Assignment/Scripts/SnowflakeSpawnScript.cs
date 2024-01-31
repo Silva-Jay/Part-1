9dd7acb9aaae8c5b9faa1ded866a66eaa91eb622
Sophia Grasman <grasman@shernet.sheridancollege.ca>
2024-01-30 22:09:42 -0500

    Commenting adjustments

--- Assets/Assignment/Scripts/SnowflakeSpawnScript.cs
+++ Assets/Assignment/Scripts/SnowflakeSpawnScript.cs
@@ -21,7 +21,9 @@ public class SnowflakeSpawnScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //teleport snowflake to another part of the map upon collision
         transform.position = new Vector3(Random.Range(-11, 11), Random.Range(-4, 1), 0);
+        //add 1 to the player's score and display it in the debug log
         score += 1;
         Debug.Log("Your score is " + score + "!");
     }
